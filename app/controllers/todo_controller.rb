6f1b2acee431fb2146c3050089dad0be3fa16794
Steven Bruno <sbruno636@gmail.com>
2018-10-20 19:06:39 +0000

    add conditional logic

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,8 +1,21 @@
 class TodoController < ApplicationController
     def index
     end
+    
     def show
-        @todo_description = "Make the curriculum"
-        @todo_pomodoro_estimate = 4
+        todo_id = params[:id]
+        
+        if todo_id == '1'
+            @todo_description = "Make the curriculum"
+            @todo_pomodoro_estimate = 4
+        
+        elsif todo_id == '2'
+            @todo_description = "Buy workshop supplies"
+            @todo_pomodoro_estimate = 3
+            
+        elsif todo_id == '3'
+            @todo_description = "name of task"
+            @todo_pomodoro_estimate = 5
+        end
     end
 end
