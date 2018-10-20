abb17f7bd65b6efcf0612157229deff65823418d
Steven Bruno <sbruno636@gmail.com>
2018-10-20 16:44:07 +0000

    add description and estimate to controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,5 +2,7 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        @todo_description = "Make the curriculum"
+        @todo_pomodoro_estimate = 4
     end
 end
